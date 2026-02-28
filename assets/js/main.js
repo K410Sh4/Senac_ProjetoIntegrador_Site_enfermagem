2c25a9d501b5820c669546e0c95b4a4b1c2bddb2
Zyphon <elgator833@gmail.com>
2026-02-27 21:49:04 -0300

    comentariojs

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,8 @@ const framecontact = document.querySelector(".contact");
 const btnclose = document.querySelector(".btnx");
 const btnopen = document.querySelector(".btnon");
 let btncounter = 0;
-
-
+framecontact.style.display = "none";
+/--o codigo nao roda a parte de estar trocando o valor do btncounter que determina se o botao está ativo ou nao--/
 function interagirbtn() {
     if (btncounter == 0) {
         btncounter = btncounter + 1
@@ -11,6 +11,7 @@ function interagirbtn() {
     else {
        btncounter = btncounter - 1
     }
+    /--aki o btn couter deveria executar conforme o seu valor o parametro dentro do seu próprio if else abaixo--/
 }
 if (btncounter == 0) {
     btnopen.style.display = "block";
@@ -20,6 +21,7 @@ else {
     btnopen.style.display = "none";
     framecontact.style.display = "block";
 }
+/--aki o btn open e btn close recebem o eventlistener de click, significando assim que apos ser clicado no sequinte elemento ele rode a funcao interagirbtn--/
 btnopen.addEventListener("click",interagirbtn);
 btnclose.addEventListener("click",interagirbtn);
 
