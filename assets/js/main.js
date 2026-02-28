051a9aa5d262641e8745251a65e662a5d5bc6699
Zyphon <elgator833@gmail.com>
2026-02-27 21:40:58 -0300

    btncontact
    /assets/js/main.js
    (animador de abrir e fechar terminal de contatos)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
-const frame = document.getElementById("contactframe");
-frame.style.display = "none";
+const framecontact = document.querySelector(".contact");
+const btnclose = document.querySelector(".btnx");
+const btnopen = document.querySelector(".btnon");
+let btncounter = 0;
 
 
-
-
+function interagirbtn() {
+    if (btncounter == 0) {
+        btncounter = btncounter + 1
+    }
+    else {
+       btncounter = btncounter - 1
+    }
+}
+if (btncounter == 0) {
+    btnopen.style.display = "block";
+    framecontact.style.display = "none";
+}
+else {
+    btnopen.style.display = "none";
+    framecontact.style.display = "block";
+}
+btnopen.addEventListener("click",interagirbtn);
+btnclose.addEventListener("click",interagirbtn);
 
