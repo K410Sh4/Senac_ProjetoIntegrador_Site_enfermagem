4a0c2ce6ca0d3ce3550595242bbfa69ebe5660d7
Zyphon <elgator833@gmail.com>
2026-03-02 21:48:45 -0300

    ....

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,19 @@ const showHome = document.querySelector("#ShowHome");
 const showParadCard = document.querySelector("#ShowHeart");
 const showEng = document.querySelector("#ShowPulms");
 const showAniPe = document.querySelector("#ShowAnimal");
+const img1 = document.querySelector(".engasgo");
+const img2 = document.querySelector(".animal");
+const img3 = document.querySelector(".coracao");
+imgs=[img1,img2,img3]
 const body = document.body;
 
 let btncounter = 0;
+let carcounter = 0;
+
+
 
 if (body.id === 'home') {
-   showHome.style.background =  "rgba(0, 0, 0, 0.3)";
+   showHome.style.background = "rgba(0, 0, 0, 0.3)";
    showHome.style.boxShadow = "2px 3px 5px white";
    showHome.style.transform = "scale(1.07)";
    showHome.style.border = "1px solid white";
@@ -29,6 +36,34 @@ if (body.id === 'home') {
    }
    btnopen.addEventListener("click", interagirbtn);
    btnclose.addEventListener("click", interagirbtn);
+
+   img1.style.width = "0vw"
+   img2.style.width = "0vw"
+   img3.style.width = "0vw"
+   img1.style.transition = "300ms"
+   img2.style.transition = "300ms"
+   img3.style.transition = "300ms"
+   if (carcounter == 0) {
+      img1.style.width = "100%"
+   }
+   if (carcounter == 1) {
+      img2.style.width = "100%"
+   }
+   if (carcounter == 2) {
+      img3.style.width = "100%"
+
+   }
+   function atualizarCarrossel() {
+      imgs.forEach((img, i) => {
+         img.style.width = (i === carcounter) ? "100%" : "0vw";
+      });
+   }
+   atualizarCarrossel();
+
+   setInterval(() => {
+      carcounter = (carcounter + 1) % imgs.length;
+      atualizarCarrossel();
+   }, 2000);
 }
 else if (body.id === 'animais') {
    showAniPe.style.background = "rgba(0, 0, 0, 0.3)";
@@ -37,13 +72,13 @@ else if (body.id === 'animais') {
    showAniPe.style.border = "1px solid white";
 }
 else if (body.id === 'coracao') {
-   showParadCard.style.background =  "rgba(0, 0, 0, 0.3)";
+   showParadCard.style.background = "rgba(0, 0, 0, 0.3)";
    showParadCard.style.boxShadow = "2px 3px 5px white";
    showParadCard.style.transform = "scale(1.07)";
    showParadCard.style.border = "1px solid white";
 }
 else if (body.id === 'engasgo') {
-   showEng.style.background =  "rgba(0, 0, 0, 0.3)";
+   showEng.style.background = "rgba(0, 0, 0, 0.3)";
    showEng.style.boxShadow = "2px 3px 5px white";
    showEng.style.transform = "scale(1.07)";
    showEng.style.border = "1px solid white";
